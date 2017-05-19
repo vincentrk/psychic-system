10e19652308953d7ec475e56a2090e17742ddfad
lars2309 <git@lars.wijtemans.nl>
2017-05-19 14:08:08 +0000

    pdf_representation and kernel to integer

--- ESLab_Assign02_Group11/code/meanshift.cpp
+++ ESLab_Assign02_Group11/code/meanshift.cpp
@@ -4,6 +4,7 @@
 */
 
 #include"meanshift.h"
+#include <limits>
 #include <algorithm>
 
 void print_mat(cv::Mat &mat)
@@ -13,6 +14,17 @@ void print_mat(cv::Mat &mat)
     for (int y=0; y<h; y++) {
         for(int x=0; x<w; x++) {
             std::cout << mat.at<float>(y, x) << " ";
+        }
+        std::cout << "\n";
+    }
+}
+void print_mat_int(cv::Mat &mat)
+{
+    int h = mat.rows;
+    int w = mat.cols;
+    for (int y=0; y<h; y++) {
+        for(int x=0; x<w; x++) {
+            std::cout << mat.at<int>(y, x) << " ";
         }
         std::cout << "\n";
     }
@@ -38,8 +50,12 @@ void  MeanShift::Init_target_frame(const cv::Mat &frame,const cv::Rect &rect)
 {
     target_Region = rect;
     float kernel_sum = Epanechnikov_kernel(kernel, rect.height, rect.width);
-    kernel /= kernel_sum; // pre-scale kernel
-    target_model = pdf_representation(frame,target_Region,0.0001);
+    kernel *= INT_MAX / kernel_sum; // pre-scale kernel
+//    print_mat(kernel);
+    kernel.convertTo(kernel, CV_32S);
+//    std::cerr << INT_MAX << " max\n";
+//    print_mat_int(kernel);
+    target_model = pdf_representation(frame,target_Region,0);
 }
 
 float & MeanShift::kernel_elem(int row, int col, int height, int width) {
@@ -66,7 +82,7 @@ float  MeanShift::Epanechnikov_kernel(cv::Mat &kernel, int h, int w)
     // Halve the size, round up, add one for the zero row
     // (x+1+1)/2 == x/2+1
     kernel.create(h/2+1, w/2+1, CV_32F);
-    std::cout << "kernel size: " << h << ";" << w << "\n";
+//    std::cout << "kernel size: " << h << ";" << w << "\n";
 
     float kernel_sum = 0.0;
     for(int i=0;i<h;i++)
@@ -86,8 +102,8 @@ float  MeanShift::Epanechnikov_kernel(cv::Mat &kernel, int h, int w)
 
 cv::Mat MeanShift::pdf_representation(const cv::Mat &frame, const cv::Rect &rect, const float init)
 {
-    std::cerr << "entering pdf_representation()\n";
-    cv::Mat pdf_model(3,cfg.num_bins,CV_32F,cv::Scalar(init));
+//    std::cerr << "entering pdf_representation()\n";
+    cv::Mat pdf_model(3,cfg.num_bins,CV_32S,cv::Scalar(init));
 
     int height = rect.height;
     int width = rect.width;
@@ -95,15 +111,15 @@ cv::Mat MeanShift::pdf_representation(const cv::Mat &frame, const cv::Rect &rect
     int kern_h = height / 2;
     int kern_w = width / 2;
 
-    float * plane_a = pdf_model.ptr<float>(0);
-    float * plane_b = pdf_model.ptr<float>(1);
-    float * plane_c = pdf_model.ptr<float>(2);
+    int * plane_a = pdf_model.ptr<int>(0);
+    int * plane_b = pdf_model.ptr<int>(1);
+    int * plane_c = pdf_model.ptr<int>(2);
 
     if (!kernel.isContinuous()) {
         std::cerr << "Error: kernel is not continuous\n";
         return pdf_model;
     }
-    float * kernel_ptr = kernel.ptr<float>(0);
+    int * kernel_ptr = kernel.ptr<int>(0);
     int kernel_row_size = kernel.cols;
 
     int row_index = rect.y;
@@ -117,10 +133,10 @@ cv::Mat MeanShift::pdf_representation(const cv::Mat &frame, const cv::Rect &rect
             bin_value[0] = (curr_pixel_value[0] >> bin_width_pow);
             bin_value[1] = (curr_pixel_value[1] >> bin_width_pow);
             bin_value[2] = (curr_pixel_value[2] >> bin_width_pow);
-            float kernel_element = kernel_ptr[abs(i - kern_h) * kernel_row_size + abs(j - kern_w)];
-            float val_a = plane_a[bin_value[0]];
-            float val_b = plane_b[bin_value[1]];
-            float val_c = plane_c[bin_value[2]];
+            int kernel_element = kernel_ptr[abs(i - kern_h) * kernel_row_size + abs(j - kern_w)];
+            int val_a = plane_a[bin_value[0]];
+            int val_b = plane_b[bin_value[1]];
+            int val_c = plane_c[bin_value[2]];
             plane_a[bin_value[0]] = val_a + kernel_element;
             plane_b[bin_value[1]] = val_b + kernel_element;
             plane_c[bin_value[2]] = val_c + kernel_element;
@@ -128,7 +144,7 @@ cv::Mat MeanShift::pdf_representation(const cv::Mat &frame, const cv::Rect &rect
         }
         row_index++;
     }
-    std::cerr << "leaving pdf_representation()\n";
+//    std::cerr << "leaving pdf_representation()\n";
     return pdf_model;
 }
 
@@ -148,39 +164,40 @@ cv::Rect MeanShift::track(const cv::Mat &next_frame)
     for(int iter=0;iter<cfg.MaxIter;iter++)
     {
         cv::Mat target_candidate = pdf_representation(next_frame,target_Region, 0);
-        float max_ratio = 0.0f;
-        std::cerr << "calculating ratio\n";
+        float max_ratio[3] = {0.0f};
+//        std::cerr << "calculating ratio\n";
         for (int k=0; k<3; k++)
         {
             for (int b=0; b<num_bins; b++)
             {
-                if (target_candidate.at<float>(k, b) == 0.0f) {
-                    target_ratio_f.at<float>(k, b) = sqrt(target_model.at<float>(k, b));
+                if (target_candidate.at<int>(k, b) == 0) {
+                    target_ratio_f.at<float>(k, b) = 0;
                 } else {
-                    target_ratio_f.at<float>(k, b) = sqrt(target_model.at<float>(k, b)
-                                             / target_candidate.at<float>(k, b));
-                    max_ratio = std::max(max_ratio, target_ratio_f.at<float>(k, b));
+                    target_ratio_f.at<float>(k, b) = sqrt(target_model.at<int>(k, b)
+                                             / target_candidate.at<int>(k, b));
+                    max_ratio[k] = std::max(max_ratio[k], target_ratio_f.at<float>(k, b));
                 }
             }
         }
-        std::cerr << "scaling...\n";
+//        std::cerr << "scaling...\n";
 
         int height = target_Region.height;
         // Loop is limited to a circle with a diameter of height
         int width = std::min(height, target_Region.width);
 
-        float limit = pow(pow(2.0, 32.0) / (height * height * height), 1.0f/3.0f);
-        float scale = std::max(limit / max_ratio, 0.0f);
-        std::cerr << "scale: " << scale << " limit: " << limit << "\n";
+        float limit = pow((INT_MAX * 2.0f * 4.0f) / (height * height * height * max_ratio[0] * max_ratio[1] * max_ratio[2]), 1.0f / 3.0f);
+        float scale = std::max(limit, 0.0f);
+//        std::cerr << "max: " << max_ratio << " scale: " << scale << " limit: " << limit << "\n";
         for (int k=0; k<3; k++)
         {
             for (int b=0; b<num_bins; b++)
             {
-                std::cerr << "ratio: " << target_ratio_f.at<float>(k, b) << "\n";
-                target_ratio.at<int>(k, b) = std::min(target_ratio_f.at<float>(k, b) * scale, limit);
+//                std::cerr << "ratioF: " << target_ratio_f.at<float>(k, b) << "\n";
+                target_ratio.at<int>(k, b) = (target_ratio_f.at<float>(k, b) * scale) + 0.5;
+//                std::cerr << "ratioI: " << target_ratio.at<int>(k, b) << "\n";
             }
         }
-        std::cerr << "converted\n";
+//        std::cerr << "converted\n";
 
         int delta_x = 0;
         int sum_wij = 0;
@@ -199,7 +216,7 @@ cv::Rect MeanShift::track(const cv::Mat &next_frame)
             const cv::Vec3b * pixels = next_frame.ptr<cv::Vec3b>(row_index) + target_Region.x;
             for(int j=0;j<width;j++)
             {
-//                std::cerr << "loop\n";
+//               std::cerr << "loop\n";
                 int norm_j = (j-centre);
                 if (norm_i_sqr + norm_j * norm_j <= centre * centre) {
                     // calculate element of weight matrix (CalWeight)
@@ -212,19 +229,22 @@ cv::Rect MeanShift::track(const cv::Mat &next_frame)
                               plane_a[bin_value[0]]
                             * plane_b[bin_value[1]]
                             * plane_c[bin_value[2]]);
-                    std::cerr << "weight: " << weight << "\n";
-                    if (weight == 0) {
-                        std::cerr << "ZERO: " << i << "," << j << " bin: " << ((int)bin_value[0]) << " " << ((int)bin_value[1]) << " " << ((int)bin_value[2]) << "\n";
-                    }
+//                    std::cerr << "weight: " << weight << "\n";
+//                    if (weight == 0) {
+//                        std::cerr << "ZERO: " << i << "," << j << " bin: " << ((int)bin_value[0]) << " " << ((int)bin_value[1]) << " " << ((int)bin_value[2]) << "\n";
+//                    }
                     delta_x += (norm_j * weight);
                     delta_y += (norm_i * weight);
+//                    if (sum_wij >= INT_MAX - weight) {
+//                        std::cerr << "overflow on sum_wij\n";
+//                    }
                     sum_wij += (weight);
                 }
             }
             row_index++;
         }
-        std::cerr << "sums: " << delta_x << " " << delta_y << " " << sum_wij << "\n";
-        std::cerr << "done\n";
+//        std::cerr << "sums: " << delta_x << " " << delta_y << " " << sum_wij << "\n";
+//        std::cerr << "done\n";
 
         next_rect.x = target_Region.x;
         next_rect.y = target_Region.y;
